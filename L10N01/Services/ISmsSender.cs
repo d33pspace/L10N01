91d4ed75a3566d2a0eb13c3bdd49ee0c38ab6092
Jimmy McWhinney <jimmy.mcw@live.com>
2017-07-30 16:39:33 +0800

    Add project files.

---
+++ L10N01/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L10N01.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
